8e167d08a7df28cba51c938597150c4244f37bed
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-05-17 21:43:08 -0300

    Add people data to JSON and update StoryCircleList to load people and stories concurrently

--- lib/widgets/story_circle_list.dart
+++ lib/widgets/story_circle_list.dart
@@ -6,46 +6,27 @@ import 'package:unifor_mobile/screens.dart';
 class StoryCircleList extends StatelessWidget {
   const StoryCircleList({super.key});
 
-  Future<List<Map<String, dynamic>>> carregarStories() async {
+  Future<List<Map<String, dynamic>>> loadStories() async {
     final String response = await rootBundle.loadString('assets/stories.json');
+    return List<Map<String, dynamic>>.from(json.decode(response));
+  }
+
+  Future<List<Map<String, dynamic>>> loadPeople() async {
+    final String response = await rootBundle.loadString('assets/people.json');
     return List<Map<String, dynamic>>.from(json.decode(response));
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> people = [
-      {
-        'userId': 1035489,
-        'name': 'Alan\nBandeira',
-        'image':
-            'https://media.licdn.com/dms/image/v2/C4E03AQFEpO5-pbHssw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1558734738354?e=1752105600&v=beta&t=3BaipvlXXkgEVxYbzAjwjKxWqBVqm-7B_pXklbocqwA',
-      },
-      {
-        'userId': 2849157,
-        'name': 'Thiago\nNarak',
-        'image':
-            'https://media.licdn.com/dms/image/v2/D4D03AQF-kEAeiNxN3Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1665756606748?e=2147483647&v=beta&t=sVk9fFvw59JCdqFq8yIIhL0oQbg7RHcJywvdSHR9w0o',
-      },
-      {'userId': 7753421, 'name': 'Maria\nJousy'},
-      {
-        'userId': 6621984,
-        'name': 'Rafaela\nPonte',
-        'image':
-            'https://media.licdn.com/dms/image/v2/C5603AQEejEvPT8DGYw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1575916280776?e=2147483647&v=beta&t=s3RTioyI5vexvXyi5julCggEdJtmlt_ivRn5oN8eFuQ',
-      },
-      {
-        'userId': 5555555,
-        'name': 'Belmondo\nRodrigues',
-        'image':
-            'https://media.licdn.com/dms/image/v2/D4D03AQG0vB6uMOvZKA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1703691760893?e=1752105600&v=beta&t=BKHFWtyThcHqjuTZ2K7p7AdUXVUQg9lQPkLBMIydmdA',
-      },
-      {'userId': 9999999, 'name': 'Francisco\nEstevao'},
-    ];
-
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: carregarStories(),
+    return FutureBuilder<List<List<Map<String, dynamic>>>>(
+      future: Future.wait([loadPeople(), loadStories()]),
       builder: (context, snapshot) {
-        final stories = snapshot.data ?? [];
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final people = snapshot.data![0];
+        final stories = snapshot.data![1];
 
         return Padding(
           padding: const EdgeInsets.only(top: 24, left: 16),
@@ -137,7 +118,7 @@ class StoryCircleList extends StatelessWidget {
                             SizedBox(
                               width: 80,
                               child: Text(
-                                person['name']!,
+                                person['name'],
                                 textAlign: TextAlign.center,
                                 style: const TextStyle(fontSize: 14),
                                 overflow: TextOverflow.ellipsis,
